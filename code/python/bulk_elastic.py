3e62080365ba90a432637901d7365c4f9d47f87a
David Fraga Rodriguez <david.fraga.rodriguez@udc.es>
2023-10-31 16:08:32 +0100

    Add encoding on open file

--- code/python/bulk_elastic.py
+++ code/python/bulk_elastic.py
@@ -1,15 +1,19 @@
+"""
+Script to generate a JSON to use as a
+body on bulk data load on Elastic index
+"""
+
 # -*- coding: utf-8 -*-
 import json
-import unicodedata
 
-with open('good_reads.json', 'r') as file:
-	data = json.load(file)
+with open('good_reads.json', 'r', encoding='utf-8') as file:
+    data = json.load(file)
 
-with open('good_reads_elastic.json', 'w') as file:
-	elastic_id = 1
-	for item in data:
-		file.write(json.dumps({"create": {"_id": elastic_id}}))
-		file.write("\n")
-		file.write(json.dumps(item))
-		file.write("\n")
-		elastic_id += 1
+with open('good_reads_elastic.json', 'w', encoding='utf-8') as file:
+    ELASTIC_ID = 1
+    for item in data:
+        file.write(json.dumps({"create": {"_id": ELASTIC_ID}}))
+        file.write("\n")
+        file.write(json.dumps(item))
+        file.write("\n")
+        ELASTIC_ID += 1
